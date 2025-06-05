e88d35215c922f3411ce963d303176cd6fd066af
Matheus Caposse <matheus.caposse02@gmail.com>
2025-06-05 13:36:43 -0300

    criando a pagina error

--- src/error/Error.jsx
+++ src/error/Error.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Error = () => {
   return (
-    <div>Error</div>
+    <div className='flex flex-col items-center'>
+
+      <img src="error-404.png" alt="imagem erro 404" className="h-auto w-70 pt-60" />
+
+      <p className='text-lg pb-10 mt-10'>Page Not Found</p>
+
+      
+
+    </div>
   )
 }
 
